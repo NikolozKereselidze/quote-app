9302ba8f9abefa4d75deb06c65a86b64f10b59e4
Nikoloz Kereselidze <kereselidze.nikoloz7@gmail.com>
2022-12-11 16:10:44 +0400

    implement programmatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteaData) => {
     console.log(quoteaData);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
